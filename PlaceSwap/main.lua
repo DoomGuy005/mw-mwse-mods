6182f243f70073542cb8ec978d231ac036832b86
Jherod Brendon <38527453+DoomGuy005@users.noreply.github.com>
2021-09-10 01:44:09 -0300

    PlaceSwap: Some refactoring

--- PlaceSwap/main.lua
+++ PlaceSwap/main.lua
@@ -1,47 +1,54 @@
-local function swapPcs(target)
-	
+local function swapPcs(ref)
 	-- gets player current cell
 	local current_cell = tes3.getPlayerCell()
-	
 	-- get copy of the player's position
 	local player_pos = tes3.mobilePlayer.position:copy()
-	
 	-- get copy of the npc's position
-	local npc_pos = target.position:copy()
-	
+	local npc_pos = ref.position:copy()
 	-- swaps both using position cell
 	tes3.positionCell(
-		{ 
-		  reference = tes3.mobilePlayer, 
-		  cell = current_cell, 
-		  position = npc_pos, 
-		  forceCellChange = false, 
-		  suppressFader = true, 
-		  teleportCompanions = false 
+		{
+		  reference = tes3.mobilePlayer,
+		  cell = current_cell,
+		  position = npc_pos,
+		  forceCellChange = false,
+		  suppressFader = true,
+		  teleportCompanions = false
 		}
 	)
 	tes3.positionCell(
 		{
-		  reference = target, 
-		  cell = current_cell, 
-		  position = player_pos, 
-		  forceCellChange = false, 
-		  suppressFader = true, 
-		  teleportCompanions = false 
+		  reference = ref,
+		  cell = current_cell,
+		  position = player_pos,
+		  forceCellChange = false,
+		  suppressFader = true,
+		  teleportCompanions = false
 		}
 	)
 end
 
-local function checkTarget(e)
-	if (e.isShiftDown) then
-		local target = tes3.getPlayerTarget()
-		if (target ~= nil and target.object.objectType == tes3.objectType.npc) then
-			if not (target.object.mobile.inCombat) then
-				swapPcs(target)
-			end
+--local function moveAway(ref)
+--	local npc = ref
+--end
+
+local function isRefValid(ref)
+	if (ref ~= nil and ref.object.objectType == tes3.objectType.npc) then
+		if not (ref.object.mobile.inCombat) then
+			return true
 		end
 	end
-	-- to do move away code
+	return false
+end
+
+local function checkTarget(e)
+	local target_ref = tes3.getPlayerTarget()
+	if (e.isShiftDown and isRefValid(target_ref)) then
+		swapPcs(target_ref)
+	end
+	--if (e.isAltDown and isRefValid(target_ref)) then
+	--	moveAway(target_ref)
+	--end
 end
 
 local function init()
